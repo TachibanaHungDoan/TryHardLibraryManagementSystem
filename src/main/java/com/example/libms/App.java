6e2c010a4fc1182ab9e8a6ae62942f2d38a509cf
TachibanaHungDoan <hungdoanthai05@gmail.com>
2024-10-26 15:15:46 +0700

    books view fxml

--- src/main/java/com/example/libms/App.java
+++ src/main/java/com/example/libms/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("MahLib");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/libms/DashBoardController.java
+++ src/main/java/com/example/libms/DashBoardController.java
@@ -1,4 +1,27 @@
 package com.example.libms;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardController {
+
+    @FXML
+    private Button booksButton;
+
+    public void booksButtonClicked(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("books-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
